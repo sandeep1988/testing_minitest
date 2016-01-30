e0be0e17023fe170b4d9335d953dfc916e8cfdd0
sandeep1988 <schourey1988@gmail.com>
2016-01-30 23:40:57 +0530

    custom create form

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,54 @@
+class ArticlesController < ApplicationController
+  # before_action :set_article, only: [:show, :edit, :update, :destroy]
+  def index
+    @articles =Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article
+    else
+      render 'articles/new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update_attributes(article_params)
+      redirect_to(@article)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    respond_to do |format|
+      format.html { redirect_to articles_url, notice: 'article was successfully destroyed.' }
+    end
+  end
+
+  private
+  # def set_article
+  #   @article = Article.find(params[:id])
+  # end
+
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
+
+end
